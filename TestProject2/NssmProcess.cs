42011c0b85c1f33885d1ba64d5254e9bd581bec5
Kevin <kevinyay945@gmail.com>
2022-07-19 16:55:52 +0800

    init

---
+++ TestProject2/NssmProcess.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace TestProject2
+{
+    public class NssmProcess
+    {
+        private readonly string _nssmPath;
+
+        public NssmProcess(string nssmPath)
+        {
+            if (nssmPath==null || nssmPath=="")
+            {
+                throw new Exception("can't get nssm.exe when initial");
+            }
+
+            this._nssmPath = nssmPath;
+        }
+
+        public string Exec(string cmd)
+        {
+            var process = new Process();
+            var stringBuilder = new StringBuilder();
+            var processStartInfo = new ProcessStartInfo();
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.RedirectStandardInput = true;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.Verb = "runas";
+            processStartInfo.Arguments = cmd;
+            processStartInfo.FileName = this._nssmPath;
+            process.StartInfo = processStartInfo;
+            process.EnableRaisingEvents = true;
+            process.OutputDataReceived += new DataReceivedEventHandler
+            (
+                delegate(object sender, DataReceivedEventArgs e)
+                {
+                    // append the new data to the data already read-in
+                    stringBuilder.Append(e.Data);
+                }
+            );
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+            process.CancelOutputRead();
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ TestProject2/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestProject2
+{
+    [TestFixture]
+    public class NssmProcessTest
+    {
+        private NssmProcess _nssmProcess;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // _nssmProcess = new NssmProcess("ipconfig.exe");
+            _nssmProcess = new NssmProcess(@"C:\Users\User\RiderProjects\TestProject2\nssm.exe");
+        }
+
+        [Test]
+        public void exec_command_at_nssm()
+        {
+            // _nssmProcess.Exec("").Should().Contain("NSSM: The non-sucking service manager");
+            _nssmProcess.Exec("remove not-exist-service confirm").Should().Contain("Can't open service!");
+        }
+    }
+}
